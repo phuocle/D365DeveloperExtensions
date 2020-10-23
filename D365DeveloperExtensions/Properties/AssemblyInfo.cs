34024e9839d0129c44dfc9642ea71327fded0d93
PhuocLe <vanphuoc@gmail.com>
2020-10-23 10:02:32 +0700

    Rebuild All succeeded

--- D365DeveloperExtensions/Properties/AssemblyInfo.cs
+++ D365DeveloperExtensions/Properties/AssemblyInfo.cs
@@ -6,18 +6,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("D365DeveloperExtensions")]
-<<<<<<< HEAD
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.0.19168.0247")]
-[assembly: AssemblyFileVersion("2.0.19168.0247")]
-=======
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.0.20239.1842")]
-[assembly: AssemblyFileVersion("2.0.20239.1842")]
->>>>>>> upstream/master
+[assembly: AssemblyVersion("2.0.20297.0302")]
+[assembly: AssemblyFileVersion("2.0.20297.0302")]
